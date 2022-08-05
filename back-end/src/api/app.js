bb13d0e8474365a912fb6a6a4fa06fcd6d12a1c2
MatheusMena <matheusmena1@hotmail.com>
2022-08-05 18:54:00 -0300

    Feat:Adiciona cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,12 +1,13 @@
 // Responsável por receber as definições de middlewares e rotas da API
 
 const express = require('express');
-
+const cors = require('cors');
 const {
   loginRouter,
 } = require('../router/index');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/login', loginRouter);
