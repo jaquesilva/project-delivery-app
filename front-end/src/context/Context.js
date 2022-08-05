f8948ee6182fbd52423fcf08cd237f65d118978d
MatheusMena <matheusmena1@hotmail.com>
2022-08-05 19:38:14 -0300

    Fix: Lint errors

--- front-end/src/context/Context.js
+++ front-end/src/context/Context.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+// import { createContext } from 'react';
 
-export const Context = createContext();
-export default Context;
+// export const Context = createContext();
+// export default Context;
