f6e4096a8e70beade6f7a760adf07f724e1a9158
martinsfaah <fabianamfranco@outlook.com>
2022-08-08 18:51:34 -0300

    linter S2

--- back-end/src/services/loginServices.js
+++ back-end/src/services/loginServices.js
@@ -2,7 +2,7 @@ const User = require('../database/models/users');
 
 const login = async (email) => {
   const loginUser = await User.findOne({ where: { email } });
-console.log(loginUser);
+  console.log(loginUser);
   if (!loginUser || undefined) {
     return 'Invalid fields';
   }
