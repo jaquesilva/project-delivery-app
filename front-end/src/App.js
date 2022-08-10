8b694cac3ba8f17fa44e39cac4c37462e4a3524a
Jaqueline Silva <jaqueline.natario@gmail.com>
2022-08-10 18:13:29 -0300

    feat: componente Navbar.jsx

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import MyRequests from './pages/MyRequests';
 import Products from './pages/Products';
 import Register from './pages/Register';
 
@@ -13,6 +14,7 @@ function App() {
       <Route exact path="/login" component={ Login } />
       <Route exact path="/customer/products" component={ Products } />
       <Route exact path="/register" component={ Register } />
+      <Route exact path="/customer/my_requests" component={ MyRequests } />
     </Switch>
   );
 }
---
+++ front-end/src/pages/MyRequests.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../components/navbar/Navbar';
+
+export default function MyRequests() {
+  return (
+    <div>
+      <Navbar />
+      <h1>My Requests</h1>
+    </div>
+  );
+}
