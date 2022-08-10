7bdfba9207b6aa1bd425b30e02974b277c1eecbc
leandrochs <leandrochagask@gmail.com>
2022-08-10 14:03:40 -0300

    feat: adiciona rota para customer em app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const {
   loginRouter,
   registerRouter,
+  customerRouter,
 } = require('../router/index');
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
+app.use('/customer', customerRouter);
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
