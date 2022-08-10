45468214b45f4d0fdef71b272535dfeeaa46f931
MatheusMena <matheusmena1@hotmail.com>
2022-08-10 19:09:22 -0300

    feat: requisição da api /costumer/products

--- front-end/src/services/requests.js
+++ front-end/src/services/requests.js
@@ -4,8 +4,14 @@ const api = axios.create({
   baseURL: 'http://localhost:3001/',
 });
 
-export default async (body) => {
+export async function requestInfo(body) {
   const result = await api.post('/login', body);
 
   return result.data;
-};
+}
+
+export async function requestProducts(endpoint) {
+  const result = await api.get(endpoint);
+
+  return result.data;
+}
