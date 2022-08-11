3327406ab866f20dee551dd2e1c56e3a6b9e3d35
leandrochs <leandrochagask@gmail.com>
2022-08-11 18:09:21 -0300

    feat: adiciona rota para orders id

--- back-end/src/router/index.js
+++ back-end/src/router/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const loginController = require('../controllers/loginControllers');
 const registerController = require('../controllers/registerController');
 const customerController = require('../controllers/customerController');
+const salesController = require('../controllers/salesController');
 
 const loginRouter = express.Router();
 loginRouter.post('/', loginController.loginUser);
@@ -12,6 +13,7 @@ registerRouter.post('/', registerController.registerUser);
 
 const customerRouter = express.Router();
 customerRouter.get('/products', customerController.customerProducts);
+customerRouter.get('/orders/:id', salesController.customerSalesOrders);
 
 module.exports = {
   loginRouter,
