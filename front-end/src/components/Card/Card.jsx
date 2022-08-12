ac856fac8984eba352738a2ba8e5a4b27b05f22b
MatheusMena <matheusmena1@hotmail.com>
2022-08-12 14:35:49 -0300

    fix(Card.jsx): Cria funçoẽs de increase e decrease

--- front-end/src/components/Card/Card.jsx
+++ front-end/src/components/Card/Card.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import './card.css';
 
-export default function Card(idProduct, title, image, price) {
+export default function Card(product) {
+  const { product: newproduct } = product;
+  const { id: idProduct, name: title, url_image: image, price } = newproduct;
+  const [priceInput, setPriceInput] = useState(0);
+  const [priceTotal, setPriceTotal] = useState((price * priceInput));
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    setPriceTotal((price * priceInput).toFixed(2));
+  }, [price, priceInput]);
+
+  const increaseQuant = () => {
+    console.log(price);
+    setPriceInput(priceInput + 1);
+    if (priceInput === 0) {
+      setCart([parseFloat(price)]);
+      console.log(cart);
+    }
+    console.log(priceInput);
+    console.log(parseFloat(price));
+    setCart([parseFloat(price) * priceInput]);
+    console.log(cart);
+  };
+  const decreaseQuant = () => {
+    if (priceInput >= 1) {
+      setPriceInput(priceInput - 1);
+      // setCart((price * priceInput).toFixed(2));
+      localStorage.setItem('carrinho', JSON.stringify(cart));
+    }
+  };
   return (
     <div
       className="card-container"
@@ -22,20 +51,24 @@ export default function Card(idProduct, title, image, price) {
       <p>product Price</p>
       <p data-testid={ `customer_products__element-card-price-${idProduct}` }>
         Valor: R$
-        {price}
+        {priceInput < 2 ? price : priceTotal}
       </p>
       <button
         type="button"
         data-testid={ `customer_products__button-card-add-item-${idProduct}` }
+        onClick={ increaseQuant }
       >
         +
       </button>
       <input
+        value={ priceInput }
         data-testid={ `customer_products__input-card-quantity-${idProduct}` }
+        onChange={ ({ target: { value } }) => setPriceInput(value) }
       />
       <button
         type="button"
         data-testid={ `customer_products__button-card-rm-item-${idProduct}` }
+        onClick={ decreaseQuant }
       >
         -
       </button>
