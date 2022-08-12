9d10e27b04a7c00a353a5a37ba60de55ac1e3097
leandrochs <leandrochagask@gmail.com>
2022-08-12 19:50:21 -0300

    fix: adiciona provider

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,17 +5,20 @@ import Login from './pages/Login';
 import CustomerOrders from './pages/CustomerOrders';
 import Products from './pages/Products';
 import Register from './pages/Register';
+import { Provider } from './context/Provider';
 
 function App() {
   return (
-    <Switch>
-      {/* <Route path="/"  { <Redirect to="/login" />}  /> */}
-      <Redirect exact from="/" to="/login" />
-      <Route exact path="/login" component={ Login } />
-      <Route exact path="/customer/products" component={ Products } />
-      <Route exact path="/register" component={ Register } />
-      <Route exact path="/customer/orders" component={ CustomerOrders } />
-    </Switch>
+    <Provider>
+      <Switch>
+        {/* <Route path="/"  { <Redirect to="/login" />}  /> */}
+        <Redirect exact from="/" to="/login" />
+        <Route exact path="/login" component={ Login } />
+        <Route exact path="/customer/products" component={ Products } />
+        <Route exact path="/register" component={ Register } />
+        <Route exact path="/customer/orders" component={ CustomerOrders } />
+      </Switch>
+    </Provider>
   );
 }
 
