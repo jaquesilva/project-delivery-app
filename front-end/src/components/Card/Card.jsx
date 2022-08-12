1fffa6612879eef44ca039b914bbb62693487376
leandrochs <leandrochagask@gmail.com>
2022-08-12 19:50:52 -0300

    refactor: adiciona logica para quantidade

--- front-end/src/components/Card/Card.jsx
+++ front-end/src/components/Card/Card.jsx
@@ -1,48 +1,41 @@
 import { useState, useEffect, useContext } from 'react';
-import { Context } from '../../context/Context';
+import Context from '../../context/Context';
 import './card.css';
 
 export default function Card(product) {
   const { buyProducts, setBuyProducts } = useContext(Context);
+
   const { product: newproduct } = product;
   const { id: idProduct, name: title, url_image: image, price } = newproduct;
-  const [priceInput, setPriceInput] = useState(0);
-  const [priceTotal, setPriceTotal] = useState((price * priceInput));
-  const [products, setProducts] = useState([priceTotal]);
 
-  console.log(buyProducts, setBuyProducts);
-  // const buyProducts = [
-  //   {name: title, unitPrice: 2.20, quantity: 2, subTotal: 4.40}
+  const [quantity, setQuantity] = useState(0);
 
   useEffect(() => {
-    // if (priceInput > 0) {
-    //   localStorage.setItem('carrinho', JSON.stringify(products));
-    // }
-  }, [price, priceInput, products]);
+    const updateBuyProducts = () => {
+      const filterBuyProducts = buyProducts.filter(
+        (p) => p.name !== title && p.quantity !== 0,
+      );
+      setBuyProducts([
+        ...filterBuyProducts,
+        {
+          name: title,
+          unitPrice: price,
+          quantity,
+          subTotal: parseFloat(price) * quantity,
+        },
+      ]);
+    };
+    updateBuyProducts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [quantity]);
 
-  const increaseQuant = () => {
-    // setBuyProducts([...buyProducts, {
-    //   name: title,
-    //   unitPrice: price,
-    //   quantity: priceInput,
-    //   subTotal: parseFloat(price) * priceInput }]);
-    setPriceInput(priceInput + 1);
-  };
-  const decreaseQuant = () => {
-    if (priceInput >= 1) {
-      setPriceInput(priceInput - 1);
-    }
-  };
   return (
     <div
       className="card-container"
       data-testid={ `customer_products__element-card-price-${idProduct}` }
     >
-      <h3
-        data-testid={ `customer_products__element-card-title-${idProduct}` }
-      >
+      <h3 data-testid={ `customer_products__element-card-title-${idProduct}` }>
         {title}
-
       </h3>
       <img
         width={ 200 }
@@ -57,26 +50,27 @@ export default function Card(product) {
       </p>
       <p>
         Valor Total:
-        { priceTotal}
+        {price * quantity}
       </p>
       <button
         type="button"
-        data-testid={ `customer_products__button-card-add-item-${idProduct}` }
-        onClick={ increaseQuant }
+        data-testid={ `customer_products__button-card-rm-item-${idProduct}` }
+        onClick={ () => setQuantity(quantity - 1) }
       >
-        +
+        -
       </button>
+
       <input
-        value={ priceInput }
+        value={ quantity }
         data-testid={ `customer_products__input-card-quantity-${idProduct}` }
-        onChange={ ({ target: { value } }) => setPriceInput(value) }
+        onChange={ ({ target: { value } }) => setQuantity(parseFloat(value)) }
       />
       <button
         type="button"
-        data-testid={ `customer_products__button-card-rm-item-${idProduct}` }
-        onClick={ decreaseQuant }
+        data-testid={ `customer_products__button-card-add-item-${idProduct}` }
+        onClick={ () => setQuantity(quantity + 1) }
       >
-        -
+        +
       </button>
     </div>
   );
