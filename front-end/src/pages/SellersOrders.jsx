ba91e4553288485dea8f3179b5e6d8dcdc98d203
MatheusMena <matheusmena1@hotmail.com>
2022-08-15 19:49:52 -0300

    fix: Comenta arquivo ainda não pronto lint

--- front-end/src/pages/SellersOrders.jsx
+++ front-end/src/pages/SellersOrders.jsx
@@ -1,73 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import getSalesBySellerId from '../services/getSalesByUserId';
+// import React, { useState, useEffect } from 'react';
+// import { useHistory } from 'react-router-dom';
+// import getSalesBySellerId from '../services/getSalesByUserId';
 
-export default function SellersOrders() {
-  const { id: sellerId } = JSON.parse(localStorage.getItem('user'));
+// export default function SellersOrders() {
+//   const history = useHistory();
+//   const { id: sellerId } = JSON.parse(localStorage.getItem('user'));
 
-  const [sales, setSales] = useState([]);
+//   const [sales, setSales] = useState([]);
 
-  async function getApi() {
-    const salesBySellerId = await getSalesBySellerId(sellerId);
-    setSales(salesBySellerId);
-  }
+//   async function getApi() {
+//     const salesBySellerId = await getSalesBySellerId(sellerId);
+//     setSales(salesBySellerId);
+//   }
 
-  function handleClickLogout() {
-    console.log('click');
-    localStorage.setItem('user', '');
-    return history.push('/login');
-  }
+//   function handleClickLogout() {
+//     console.log('click');
+//     localStorage.setItem('user', '');
+//     history.push('/login');
+//   }
 
-  useEffect(() => {
-    getApi();
-  }, []);
+//   useEffect(() => {
+//     getApi();
+//   }, [getApi]);
 
-  return (
-    <div>
+//   return (
+//     <div>
 
-      <nav className="navbar-container">
-        <h1>
-          PEDIDOS
-        </h1>      
-        <div data-testid="">
-          {localStorage.getItem('user') ? getLocalStorageName() : null}
-        </div>
-        <button
-          type="button"
-          data-testid="customer_products__element-navbar-link-logout"
-          onClick={ () => handleClickLogout() }
-        >
-          SAIR
-        </button>
-      </nav>
+//       <nav className="navbar-container">
+//         <h1>
+//           PEDIDOS
+//         </h1>
+//         <div data-testid="">
+//           {localStorage.getItem('user') ? getLocalStorageName() : null}
+//         </div>
+//         <button
+//           type="button"
+//           data-testid="customer_products__element-navbar-link-logout"
+//           onClick={ () => handleClickLogout() }
+//         >
+//           SAIR
+//         </button>
+//       </nav>
 
-      <div> CARDS C/ PEDIDOS </div>
-      {sales.map(({ id, status, saleDate, totalPrice, deliveryAddress, deliveryNumber }) => {
-        console.log(saleDate, totalPrice);
-        return (
-          <div key={ id }>
-            <div data-testid={ `seller_orders__element-order-id-${id}` }>
-              {id}
-            </div>
-            <div data-testid={ `seller_orders__element-delivery-status-${id}` }>
-              {status}
-            </div>
-            <div data-testid={ `seller_orders__element-order-date-${id}` }>
-              {saleDate}
-            </div>
-            <div data-testid={ `seller_orders__element-card-price-${id}` }>
-              {totalPrice}
-            </div>
-            <div data-testid={ `seller_orders__element-card-address-${id}` }>
-              {deliveryAddress}
-            </div>
-            <div data-testid={ `seller_orders__element-card-address-${id}` }>
-              {deliveryNumber}
-            </div>
-          </div>
-        );
-      })}
-      {sales.length === 0 && <div>Nenhuma venda por aqui!</div>}
+//       <div> CARDS C/ PEDIDOS </div>
+//       {sales
+//         .map(({ id, status, saleDate, totalPrice, deliveryAddress, deliveryNumber }) => {
+//           console.log(saleDate, totalPrice);
+//           return (
+//             <div key={ id }>
+//               <div data-testid={ `seller_orders__element-order-id-${id}` }>
+//                 {id}
+//               </div>
+//               <div data-testid={ `seller_orders__element-delivery-status-${id}` }>
+//                 {status}
+//               </div>
+//               <div data-testid={ `seller_orders__element-order-date-${id}` }>
+//                 {saleDate}
+//               </div>
+//               <div data-testid={ `seller_orders__element-card-price-${id}` }>
+//                 {totalPrice}
+//               </div>
+//               <div data-testid={ `seller_orders__element-card-address-${id}` }>
+//                 {deliveryAddress}
+//               </div>
+//               <div data-testid={ `seller_orders__element-card-address-${id}` }>
+//                 {deliveryNumber}
+//               </div>
+//             </div>
+//           );
+//         })}
+//       {sales.length === 0 && <div>Nenhuma venda por aqui!</div>}
 
-    </div>
-  )
-};
+//     </div>
+//   );
+// }
