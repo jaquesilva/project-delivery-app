d4c8481756ef549b3ae72dec2b9db85107fe0f1e
leandrochs <leandrochagask@gmail.com>
2022-08-17 19:11:14 -0300

    refactor: altera rotas para customer orders

--- back-end/src/router/index.js
+++ back-end/src/router/index.js
@@ -15,7 +15,8 @@ registerRouter.post('/', registerController.registerUser);
 
 const customerRouter = express.Router();
 customerRouter.get('/products', customerController.customerProducts);
-customerRouter.get('/orders/:id', salesController.customerSalesOrders);
+customerRouter.get('/orders/:saleId', salesController.customerSalesOrders);
+customerRouter.post('/orders', salesController.getByUserId);
 customerRouter.post('/checkout', authMiddleware, salesController.customerCheckout);
 
 const sellerRouter = express.Router();
